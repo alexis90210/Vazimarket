71a31fca681dcf98b2760b2662623d5ca97523f4
alexis90210 <alexisng90210@gmail.com>
2023-01-27 01:43:14 +0100

    asset integration -Home

---
+++ public/main.js
@@ -0,0 +1,14 @@
+// WebFontConfig
+
+WebFontConfig = {
+  google: {
+    families: ["Poppins:300,400,500,600,700,800"],
+  },
+};
+(function (d) {
+  var wf = d.createElement("script"),
+    s = d.scripts[0];
+  wf.src = "./theme/js/webfont.js";
+  wf.async = true;
+  s.parentNode.insertBefore(wf, s);
+})(document);
